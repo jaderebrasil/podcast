d3a45e79473ea6bf8aa8dd765360eac227cc7a8f
Jader Brasil <jaderebrasil@gmail.com>
2019-10-09 15:20:22 -0300

    adding itunes:episode

--- item.go
+++ item.go
@@ -47,6 +47,7 @@ type Item struct {
 	IExplicit          string `xml:"itunes:explicit,omitempty"`
 	IIsClosedCaptioned string `xml:"itunes:isClosedCaptioned,omitempty"`
 	IOrder             string `xml:"itunes:order,omitempty"`
+	IEpisode           string `xml:"itunes:episode,omitempty"`
 }
 
 // AddEnclosure adds the downloadable asset to the podcast Item.
